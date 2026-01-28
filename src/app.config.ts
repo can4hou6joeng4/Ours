9543cec80886c0d5a791c9353b7221fd7e9458cd
can4hou6joeng4 <can4hou6joeng4@163.com>
2026-01-29 01:14:35 +0800

    build: 更新项目依赖并注册新页面路由

---
+++ src/app.config.ts
@@ -0,0 +1,37 @@
+export default defineAppConfig({
+  pages: [
+    'pages/index/index',
+    'pages/store/index',
+    'pages/history/index',
+    'pages/me/index',
+    'pages/inventory/index',
+    'pages/binding/index'
+  ],
+  window: {
+    backgroundTextStyle: 'light',
+    navigationBarBackgroundColor: '#fff',
+    navigationBarTitleText: 'Ours',
+    navigationBarTextStyle: 'black',
+    backgroundColor: '#F8F9FE'
+  },
+  tabBar: {
+    color: '#6A6A6A',
+    selectedColor: '#7B61FF',
+    backgroundColor: '#ffffff',
+    borderStyle: 'white',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '任务'
+      },
+      {
+        pagePath: 'pages/store/index',
+        text: '商城'
+      },
+      {
+        pagePath: 'pages/me/index',
+        text: '我的'
+      }
+    ]
+  }
+})
